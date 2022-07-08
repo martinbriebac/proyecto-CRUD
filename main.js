c46c33e4f47e4ee6738683df503554432b27a674
MartinBrieba <martin.brieba@gmail.com>
2022-07-08 19:40:12 -0400

    creé documento html, css y js

---
+++ main.js
@@ -0,0 +1,30 @@
+let listaTareas = []
+
+function agregaTarea(tarea, responsable){
+    let id = listaTareas[listaTareas.length-1]?.id || 0;
+    let nuevaTarea = {
+        id: id+1,
+        tarea: tarea,
+        responsable: responsable,
+    };
+    listaTareas.push(nuevaTarea)
+    console.log(listaTareas)
+}
+
+function imprimeTarea() {
+    console.log(listaTareas)
+}
+
+function editaTarea(id, tarea, responsable){
+    listaTareas.forEach((element) => {
+        if(id === element.id){
+            element.tarea = tarea,
+            element.responsable = responsable
+        }
+    })
+}
+
+function eliminarTarea(id){
+    let filtrado = listaTareas.filter(element => element.id !== id)
+    listaTareas = filtrado
+}

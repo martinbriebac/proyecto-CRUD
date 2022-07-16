a95b276b10fdb60208fc33822c43530a1e5c4b93
MartinBrieba <martin.brieba@gmail.com>
2022-07-15 22:43:49 -0400

    borré el checkbox input

--- main.js
+++ main.js
@@ -18,14 +18,8 @@ document.addEventListener("DOMContentLoaded", function(event){
             <th scope="row">${index+1}</th>
             <td>${tarea.responsable}</td>
             <td>${tarea.tarea}</td>
-            <td>
-            <div class="form-check form-switch justify-content-end">
-            <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault">
-            <label class="form-check-label" for="flexSwitchCheckDefault">Tarea completada</label>
-            </div>
-            </td>
             <td><button type="button" class="btn btn-primary editar"><i class="fa-regular fa-pen-to-square"></i></button></td>
-            <td><button type="button" class="btn btn-danger borrar" id=${tarea.id}><i class="fa-regular fa-trash-can"></i></button></td>
+            <td><button type="button" class="btn btn-danger borrar" id="${tarea.id}"><i class="fa-regular fa-trash-can"></i></button></td>
             </tr>
             `
             
@@ -45,12 +39,12 @@ document.addEventListener("DOMContentLoaded", function(event){
     
     function agregaTarea(event){
         event.preventDefault()
-        // let listaGuardada = localStorage.getItem('lista')
-        // if(listaGuardada == null){
-        //     listaTareas = []
-        // }else{
-        //     listaTareas = JSON.parse(listaGuardada)
-        // }
+        let listaGuardada = localStorage.getItem('lista')
+        if(listaGuardada == null){
+            listaTareas = []
+        }else{
+            listaTareas = JSON.parse(listaGuardada)
+        }
         let id = listaTareas[listaTareas.length-1]?.id || 0;
         let nuevaTarea = {
             id: id+1,
@@ -66,10 +60,11 @@ document.addEventListener("DOMContentLoaded", function(event){
     
     let botonBorrar = Array.from(document.getElementsByClassName('btn btn-danger borrar'))
     botonBorrar.forEach((button)=>{
-        button.addEventListener('click', (event) => borrarTarea(event.target.id))
+        botonBorrar.addEventListener('click', (event) => console.log(event.target.getAttribute('elemento')))
     })
     
     function borrarTarea(id){
+        botonBorrar.setAttribute('elemento', id)
         let listaGuardada = localStorage.getItem('lista')
         if(listaGuardada == null){
             listaTareas = []
